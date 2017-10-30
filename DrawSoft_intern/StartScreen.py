835ee031d707f2491fd496e7c3da828b7fd8efe7
yayoi K <15j5024@stu.meisei-u.ac.jp>
2017-10-30 11:16:39 +0900

    add StartScreen

--- DrawSoft_intern/StartScreen.py
+++ DrawSoft_intern/StartScreen.py
@@ -4,13 +4,14 @@ from DrawSoftMain import Screen
 
 class StartScreen:
     def __init__(self):
+        img_start = cv2.imread('DrawSoftStartScreen.png')
         startScreen = np.tile(np.uint8([255, 255, 255]), (Screen().height(), Screen().width(), 1))
         fontType = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(startScreen, 'push a',(int(Screen().width()/4),int(Screen().height()/2)), fontType, 10, (0, 0, 0), 20, -1)
-        cv2.imshow('DrawSoft',startScreen)
+        #cv2.imshow('DrawSoft',startScreen)
+        cv2.imshow('DrawSoft',img_start)
 
         while True:
             key = cv2.waitKey(100)
             if key is ord('a'):
                 break
-         
